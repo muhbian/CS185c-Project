bf35d3dec163293805b88aa0f33096d4a203cc86
xTimpi <tim@pissarczyk.com>
2014-10-24 17:19:51 -0700

    init

---
+++ Assets/Scripts/crate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crate : MonoBehaviour {
+	public enum ContentTypes{Ammunition, PogoCharges, Score};
+
+	public ContentTypes content;
+	public int value;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.tag == "Player") {
+			switch(this.content) {
+			case ContentTypes.Ammunition:
+				c.SendMessage("addAmmunition",value);
+				break;
+			case ContentTypes.PogoCharges:
+				c.SendMessage("addPogoCharges",value);
+				break;
+			case ContentTypes.Score:
+				c.SendMessage("addScore",value);
+				break;
+			default:
+				break;
+			} 
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public float speed;
+	public float jumpstrenght;
+	public float pogoStrength;
+	public float gravity;
+	public bool onPogo;
+
+	private Animator anim;
+	private CharacterController p;
+	private Vector3 dir = new Vector3 ();
+	private int ammunition = 5;
+	private int pogoCharges = 5;
+	private int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = (Animator)this.GetComponent ("Animator");
+		p = (CharacterController)(this.GetComponent("CharacterController"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Handle Movement
+		dir.x = Input.GetAxis ("Horizontal") * speed;
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.rotation = new Quaternion(0,180,0,0);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			transform.rotation = new Quaternion(0,0,0,0);
+		}
+		if (Input.GetButtonDown ("Jump") && p.isGrounded) {
+			dir.y = jumpstrenght;
+		}
+		if (Input.GetButtonDown ("Pogo") && p.isGrounded && this.pogoCharges > 0) {
+			dir.y = pogoStrength;
+			this.pogoCharges--;
+			this.onPogo = true;
+			anim.SetBool ("onPogo", true);
+		}
+		dir.y -= gravity * Time.deltaTime;
+		p.Move (dir * Time.deltaTime);
+
+		// Handle Shooting
+		if (Input.GetButtonDown ("Fire1")) {
+			anim.SetTrigger("shot");
+		}
+
+		// Handle Animations
+		if (dir.x != 0) {
+			anim.SetBool ("moving", true);
+		} else {
+			anim.SetBool ("moving", false);
+		}
+
+		if (p.isGrounded) {
+			anim.SetBool ("isGrounded", true);
+			anim.SetBool ("onPogo", false);
+			this.onPogo = false;
+		} else {
+			anim.SetBool ("isGrounded", false);
+		}
+	}
+
+	void addAmmunition(int amount) {
+		this.ammunition += amount;
+		Debug.Log (this.ammunition);
+	}
+
+	void addPogoCharges(int amount) {
+		this.pogoCharges += amount;
+		Debug.Log (this.pogoCharges);
+	}
+
+	void addScore(int amount) {
+		this.score += amount;
+		Debug.Log (this.score);
+	}
+}
