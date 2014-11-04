478b69bbe12d96832fbf8ebc237565d9c5402b8f
Fabian Schumann <fschumann@hs-bremen.de>
2014-11-04 14:35:48 -0800

    arranged Labels on Gui

--- Assets/Scripts/GUI_script.cs
+++ Assets/Scripts/GUI_script.cs
@@ -14,7 +14,6 @@ public class GUI_script : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		guiStyle = new GUIStyle ();
 		guiStyle.normal.textColor = Color.white;
 		guiStyle.fontSize = 12;
 
@@ -24,17 +23,19 @@ public class GUI_script : MonoBehaviour {
 
 		// Scores
 		// GUI.Label (new Rect (10, 20, 16, 16), scoreTxtr, guiStyle);
-		string scoreLabel = "Score: " + this.player.score.ToString ();
-		GUI.Label (new Rect (10, 35, 20, 20), scoreLabel, guiStyle);
+		string scoreLabel = "Score:";
+		GUI.Label (new Rect (10, 32, 20, 20), scoreLabel, guiStyle);
+		string score = this.player.score.ToString ();;
+		GUI.Label (new Rect (85, 32, 20, 20), score, guiStyle);
 		// Lives 16x16
-		GUI.Label (new Rect (10, 10, 16, 16), livesTxtr, guiStyle);
+		GUI.Label (new Rect (10, 8, 16, 16), livesTxtr, guiStyle);
 		string livesLabel = this.player.lives.ToString ();
-		GUI.Label (new Rect (28, 10, 20, 20), livesLabel, guiStyle);
+		GUI.Label (new Rect (30, 10, 20, 20), livesLabel, guiStyle);
 		// Ammo
 		if(this.player.hasGun) {
-			GUI.Label (new Rect (40, 53, 32, 20), ammoTxtr, guiStyle);
+			GUI.Label (new Rect (47, 13, 32, 20), ammoTxtr, guiStyle);
 			string ammoLabel = this.player.ammunition.ToString ();
-			GUI.Label (new Rect (58, 50, 20, 20), ammoLabel, guiStyle);
+			GUI.Label (new Rect (69, 10, 20, 20), ammoLabel, guiStyle);
 		}
 	}
 
